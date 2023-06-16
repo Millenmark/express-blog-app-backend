6bd380fe21fc2d3b95852dd3f8c77dfc5ac730c7
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-06-16 10:10:33 +0800

    updates my usercontroller file

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,7 +1,34 @@
-export const registerUser = async (req, res) => {
+import User from "../models/User.js";
+
+export const registerUser = async (req, res, next) => {
   try {
     const { name, email, password } = req.body;
 
     // check whether the user exists or not
-  } catch (error) {}
+    let user = await User.findOne({ email });
+
+    if (user) {
+      // return res.status(400).json({ message: "User have already registered!" });
+      throw new Error("User already registered!");
+    }
+
+    // create new user
+    user = await User.create({
+      name,
+      email,
+      password,
+    });
+
+    return res.status(201).json({
+      _id: user._id,
+      avatar: user.avatar,
+      name: user.name,
+      email: user.email,
+      isVerified: user.isVerified,
+      isAdmin: user.isAdmin,
+      token: await user.generateJWT(),
+    });
+  } catch (error) {
+    next(error);
+  }
 };
