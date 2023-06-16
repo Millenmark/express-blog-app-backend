e4afbf64dcb23affd2a22de8a879e42f308f5b0a
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-06-16 10:10:19 +0800

    imports custom middleware

--- src/index.js
+++ src/index.js
@@ -2,20 +2,28 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectToDatabase from "./config/database.js";
+import {
+  errorResponserHandler,
+  invalidPathHandler,
+} from "./middleware/errorHandler.js";
 // Routes import
 import userRoute from "./routes/userRoutes.js";
 
-//initialize
+// initialize
 dotenv.config();
 connectToDatabase();
 const app = express();
 
-//middleware before routes
+// middleware before routes
 app.use(express.json());
 app.use(cors());
 
-//routes
+// routes
 app.use("/api/users", userRoute);
+
+// middleware after routes
+app.use(invalidPathHandler);
+app.use(errorResponserHandler);
 
 app.listen(5000, () => {
   console.log("Server is already running...");
