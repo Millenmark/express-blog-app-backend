ff0d133b03e76f248bf64979396e510bed57e46f
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-06-20 09:52:40 +0800

    created a loginUser function

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -32,3 +32,31 @@ export const registerUser = async (req, res, next) => {
     next(error);
   }
 };
+
+export const loginUser = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    let user = await User.findOne({ email });
+
+    if (!user) {
+      throw new Error("Email not found");
+    }
+
+    if (await user.comparePassword(password)) {
+      return res.status(201).json({
+        _id: user._id,
+        avatar: user.avatar,
+        name: user.name,
+        email: user.email,
+        isVerified: user.isVerified,
+        isAdmin: user.isAdmin,
+        token: await user.generateJWT(),
+      });
+    } else {
+      throw new Error("Invalid Email or Password.");
+    }
+  } catch (error) {
+    next(error);
+  }
+};
