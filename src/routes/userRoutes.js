9e0b188dff8ac2742fb1aa335b21236555477617
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-06-20 10:45:44 +0800

    added authGuard middleware in /profile route

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,10 +1,17 @@
 import { Router } from "express";
 // Controllers import
-import { loginUser, registerUser } from "../controllers/userControllers.js";
+import {
+  loginUser,
+  registerUser,
+  userProfile,
+} from "../controllers/userControllers.js";
+
+import { authGuard } from "../middleware/authMiddleware.js";
 
 const userRoute = Router();
 
 userRoute.post("/register", registerUser);
 userRoute.post("/login", loginUser);
+userRoute.get("/profile", authGuard, userProfile);
 
 export default userRoute;
