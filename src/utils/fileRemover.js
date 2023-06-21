a1b2eb021202e0650ee9fd1d73f5a9bc8cf9a93c
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-06-21 18:42:15 +0800

    created: fileRemover function

---
+++ src/utils/fileRemover.js
@@ -0,0 +1,17 @@
+import fs from "fs";
+import path from "path";
+
+const fileRemover = (filename) => {
+  fs.unlink(path.join(__dirname, "../uploads", filename), function (err) {
+    if (err && err.code == "ENOENT") {
+      //file doesn't exist
+      console.log(`File ${filename} doesn't exist.`);
+    } else if (err) {
+      console.log(`An error occurred while trying to remove file ${filename}`);
+    } else {
+      console.log(`${filename} successfully removed.`);
+    }
+  });
+};
+
+export default fileRemover;
