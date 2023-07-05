74765cfa77e023cff6a2b9f447feac5fb0dcb6e3
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-07-05 11:57:03 +0800

    Update PostCategory.js

--- src/models/PostCategory.js
+++ src/models/PostCategory.js
@@ -1,23 +1,14 @@
 import { Schema, model } from "mongoose";
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
 
 const PostCategorySchema = new Schema(
   {
-    title: { type: String, required: true },
-    caption: { type: String, required: true },
-    slug: { type: String, required: true, unique: true },
-    body: { type: Object, required: true },
-    photo: { type: String, required: true },
-    user: { type: Schema.Types.ObjectId, ref: "User" },
-    tags: { type: [String] },
-    categories: [{ type: Schema.Types.ObjectId, ref: "PostCategories" }],
+    name: { type: String, required: true },
   },
   {
     timestamps: true,
   }
 );
 
-const PostCategory = model("post", PostCategorySchema);
+const PostCategory = model("postCategory", PostCategorySchema);
 
 export default PostCategory;
