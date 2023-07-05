75b1f009a9ba78b98d01502e62a6d20f8f33d3b0
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-07-05 11:55:21 +0800

    Create Post.js

---
+++ src/models/Post.js
@@ -0,0 +1,21 @@
+import { Schema, model } from "mongoose";
+
+const PostSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    caption: { type: String, required: true },
+    slug: { type: String, required: true, unique: true },
+    body: { type: Object, required: true },
+    photo: { type: String, required: true },
+    user: { type: Schema.Types.ObjectId, ref: "user" },
+    tags: { type: [String] },
+    categories: [{ type: Schema.Types.ObjectId, ref: "postCategory" }],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Post = model("post", PostSchema);
+
+export default Post;
